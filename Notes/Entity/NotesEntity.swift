bd980f0c52b67aa9fde4736b878213c41b3355d7
Pelin AYBAR <aybarpelin00@gmail.com>
2023-09-19 15:57:40 +0300

    FourthCommit

--- Notes/Entity/NotesEntity.swift
+++ Notes/Entity/NotesEntity.swift
@@ -1,1 +1,13 @@
 import Foundation
+
+class Notes{
+    var notes_id:String?
+    var notes_title:String?
+    var notes_detail:String?
+    
+    init(notes_id:String,notes_title:String,notes_detail:String){
+        self.notes_id = notes_id
+        self.notes_title = notes_title
+        self.notes_detail = notes_detail
+    }
+}
--- Notes/Notes_Home/View/HomeViewController.swift
+++ Notes/Notes_Home/View/HomeViewController.swift
@@ -2,8 +2,21 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var notesTableView: UITableView!
+    
+    var notesLists = [Notes]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 }
-
+extension HomeViewController: UISearchBarDelegate{
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchText == "" {
+            print("iffff")
+        }else{
+            print("elseee")
+        }
+    }
+}
